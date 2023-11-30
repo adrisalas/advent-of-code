a75edc1249bb715270d7c1df4a533e8bc1b6efda
adrisalas <45205815+adrisalas@users.noreply.github.com>
2023-11-30 16:31:58 +0100

    Add modifications to template

--- src/Utils.kt
+++ src/Utils.kt
@@ -3,19 +3,16 @@ import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
-/**
- * Reads lines from the given input txt file.
- */
-fun readInput(name: String) = Path("src/$name.txt").readLines()
+fun readInput(name: String): List<String> {
+    return Path("src/$name.txt").readLines()
+}
 
-/**
- * Converts string to md5 hash.
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+fun String.md5(): String {
+    return BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
+        .toString(16)
+        .padStart(32, '0')
+}
 
-/**
- * The cleaner shorthand for printing output.
- */
-fun Any?.println() = println(this)
+fun Any?.println() {
+    println(this)
+}
