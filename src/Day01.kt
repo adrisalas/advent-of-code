8e549b8c4d4235ab8c846b7c852752ca38df0cc9
adrisalas <45205815+adrisalas@users.noreply.github.com>
2023-12-01 10:56:31 +0100

    Refactor Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -20,46 +20,45 @@ private fun part1(input: List<String>): Int {
  *
  * This can be avoided finding the "first" match starting from the last part of the string.
  *
- * See: [getLastMatch]
+ * See: [Regex.lastMatch]
  */
 private fun part2(input: List<String>): Int {
     return input.sumOf { line ->
         val regex = "[0-9]|one|two|three|four|five|six|seven|eight|nine".toRegex()
 
-        val firstDigit = getFirstMatch(regex, line)
-        val lastDigit = getLastMatch(regex, line)
+        val firstDigit = regex.firstMatch(line)?.toSingleDigit() ?: 0
+        val lastDigit = regex.lastMatch(line)?.toSingleDigit() ?: 0
 
         return@sumOf "$firstDigit$lastDigit".toInt()
     }
 }
 
-private fun getFirstMatch(regex: Regex, line: String): Char {
-    val number = regex.find(line)?.value
-    return number?.fromNumberWordToDigit() ?: '0'
+private fun Regex.firstMatch(line: String): String? {
+    return this.find(line)?.value
 }
 
-private fun getLastMatch(regex: Regex, line: String): Char {
-    var number: String? = null
+private fun Regex.lastMatch(line: String): String? {
+    var lastMatch: String? = null
     var position = line.length
-    while (number == null && position >= 0) {
-        number = regex.find(line, position)?.value
+    while (lastMatch == null && position >= 0) {
+        lastMatch = this.find(line, position)?.value
         position--
     }
-    return number?.fromNumberWordToDigit() ?: '0'
+    return lastMatch
 }
 
-fun String.fromNumberWordToDigit(): Char {
+fun String.toSingleDigit(): Int {
     return when (this) {
-        "0", "zero" -> '0'
-        "1", "one" -> '1'
-        "2", "two" -> '2'
-        "3", "three" -> '3'
-        "4", "four" -> '4'
-        "5", "five" -> '5'
-        "6", "six" -> '6'
-        "7", "seven" -> '7'
-        "8", "eight" -> '8'
-        "9", "nine" -> '9'
+        "0", "zero" -> 0
+        "1", "one" -> 1
+        "2", "two" -> 2
+        "3", "three" -> 3
+        "4", "four" -> 4
+        "5", "five" -> 5
+        "6", "six" -> 6
+        "7", "seven" -> 7
+        "8", "eight" -> 8
+        "9", "nine" -> 9
         else -> error("Invalid input")
     }
 }
