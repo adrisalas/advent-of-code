12a97709a058469d656a643ca61b697c095fb4a1
adrisalas <45205815+adrisalas@users.noreply.github.com>
2023-12-01 10:26:32 +0100

    Add modifications to template

--- src/Day00.kt
+++ src/Day00.kt
@@ -1,12 +1,12 @@
+private fun part1(input: List<String>): Int {
+    return input.size
+}
+
+private fun part2(input: List<String>): Int {
+    return input.size
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day00_test")
     check(part1(testInput) == 1)
