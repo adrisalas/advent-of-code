3ab9772a81a047e59e1387152914266e7a418c48
adrisalas <45205815+adrisalas@users.noreply.github.com>
2023-12-03 15:08:03 +0100

    Refactor Day00 template

--- src/main/kotlin/year2023/Day00.kt
+++ src/main/kotlin/year2023/Day00.kt
@@ -1,18 +1,18 @@
 package year2023
 
-private fun part1(input: List<String>): Int {
-    return input.size
-}
-
-private fun part2(input: List<String>): Int {
-    return input.size
-}
-
 fun main() {
-    val testInput = readInput("Day00_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day00")
-    part1(input).println()
-    part2(input).println()
+    Day00.part1(input).println()
+    Day00.part2(input).println()
 }
+
+object Day00 {
+    fun part1(input: List<String>): Int {
+        return input.size
+    }
+
+    fun part2(input: List<String>): Int {
+        return input.size
+    }
+}
+
