248195f828e655e81ecc79a58d0a0412357e74ba
adrisalas <45205815+adrisalas@users.noreply.github.com>
2023-12-11 08:30:05 +0100

    Refactor

--- src/test/kotlin/year2023/Day10Test.kt
+++ src/test/kotlin/year2023/Day10Test.kt
@@ -4,72 +4,71 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import kotlin.test.Test
 
 class Day10Test {
-
-    var firstLoop = listOf(
-        ".....",
-        ".S-7.",
-        ".|.|.",
-        ".L-J.",
-        "....."
-    )
-
-    var secondLoop = listOf(
-        "..F7.",
-        ".FJ|.",
-        "SJ.L7",
-        "|F--J",
-        "LJ..."
-    )
-
-    var thirdLoop = listOf(
-        "...........",
-        ".S-------7.",
-        ".|F-----7|.",
-        ".||.....||.",
-        ".||.....||.",
-        ".|L-7.F-J|.",
-        ".|..|.|..|.",
-        ".L--J.L--J.",
-        "..........."
-    )
-
-    val fourthLoop = listOf(
-        "FF7FSF7F7F7F7F7F---7",
-        "L|LJ||||||||||||F--J",
-        "FL-7LJLJ||||||LJL-77",
-        "F--JF--7||LJLJ7F7FJ-",
-        "L---JF-JLJ.||-FJLJJ7",
-        "|F|F-JF---7F7-L7L|7|",
-        "|FFJF7L7F-JF7|JL---7",
-        "7-L-JL7||F7|L7F-7F7|",
-        "L.L7LFJ|||||FJL7||LJ",
-        "L7JLJL-JLJLJL--JLJ.L"
-    )
-
     @Test
-    fun part1() {
-        val output = Day10.part1(firstLoop)
+    fun `Part 1, example 1`() {
+        val fileContent = listOf(
+            ".....",
+            ".S-7.",
+            ".|.|.",
+            ".L-J.",
+            "....."
+        )
+
+        val output = Day10.part1(fileContent)
 
         assertEquals(4, output)
     }
 
     @Test
-    fun part1_secondLoop() {
-        val output = Day10.part1(secondLoop)
+    fun `Part 1, example 2`() {
+        val fileContent = listOf(
+            "..F7.",
+            ".FJ|.",
+            "SJ.L7",
+            "|F--J",
+            "LJ..."
+        )
+
+        val output = Day10.part1(fileContent)
 
         assertEquals(8, output)
     }
 
     @Test
-    fun part2() {
-        val output = Day10.part2(thirdLoop)
+    fun `Part 2, example 1`() {
+        val fileContent = listOf(
+            "...........",
+            ".S-------7.",
+            ".|F-----7|.",
+            ".||.....||.",
+            ".||.....||.",
+            ".|L-7.F-J|.",
+            ".|..|.|..|.",
+            ".L--J.L--J.",
+            "..........."
+        )
+
+        val output = Day10.part2(fileContent)
 
         assertEquals(4, output)
     }
 
     @Test
-    fun part2_fourthLoop() {
-        val output = Day10.part2(fourthLoop)
+    fun `Part 2, example 2`() {
+        val fileContent = listOf(
+            "FF7FSF7F7F7F7F7F---7",
+            "L|LJ||||||||||||F--J",
+            "FL-7LJLJ||||||LJL-77",
+            "F--JF--7||LJLJ7F7FJ-",
+            "L---JF-JLJ.||-FJLJJ7",
+            "|F|F-JF---7F7-L7L|7|",
+            "|FFJF7L7F-JF7|JL---7",
+            "7-L-JL7||F7|L7F-7F7|",
+            "L.L7LFJ|||||FJL7||LJ",
+            "L7JLJL-JLJLJL--JLJ.L"
+        )
+
+        val output = Day10.part2(fileContent)
 
         assertEquals(10, output)
     }
