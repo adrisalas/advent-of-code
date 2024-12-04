e2997ae9e37d643dc52dc741b37737d00cc0f0fa
adrisalas <45205815+adrisalas@users.noreply.github.com>
2024-12-04 16:03:19 +0100

    Refactor Day03

--- golang/2024/day03/main.go
+++ golang/2024/day03/main.go
@@ -21,13 +21,12 @@ func part1(text string) int {
 }
 
 func sumAllMulInstructions(text string) int {
-	re, _ := regexp.Compile(`mul\(\d{1,3},\d{1,3}\)`)
+	re, _ := regexp.Compile(`mul\((\d{1,3}),(\d{1,3})\)`)
 
 	sum := 0
-	for _, match := range re.FindAllString(text, -1) {
-		aux := strings.Split(strings.Split(match, "mul(")[1], ",")
-		x1, _ := strconv.Atoi(aux[0])
-		x2, _ := strconv.Atoi(strings.Split(aux[1], ")")[0])
+	for _, match := range re.FindAllStringSubmatch(text, -1) {
+		x1, _ := strconv.Atoi(match[1])
+		x2, _ := strconv.Atoi(match[2])
 
 		sum += x1 * x2
 	}
